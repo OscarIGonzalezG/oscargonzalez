8aed00e1a224dbe8a91c7427edcef3da2ee67fd0
Oscar <oscargonzalez.gon91@gmail.com>
2025-02-17 13:01:59 -0300

    actualizacion de formulario

--- js/enviarForm.js
+++ js/enviarForm.js
@@ -4,17 +4,17 @@ document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Sending...';
+   btn.value = 'Enviando Mensaje...';
 
    const serviceID = 'default_service';
    const templateID = 'template_pbf50r9';
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-      btn.value = 'Send Email';
-      alert('Enviado!');
+      btn.value = 'Enviar Mensaje';
+      swal("Mensaje Enviado!", "Te contactare a la brevedad", "success" );
     }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
+      btn.value = 'Enviar Mensaje';
+      swal(JSON.stringify(err, 'error'));
     });
 });
